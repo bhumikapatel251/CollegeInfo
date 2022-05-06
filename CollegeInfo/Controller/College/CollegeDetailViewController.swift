652900b8a6b4325b093b7a02524170bf05c7e2d8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-06 17:03:08 +0530

    TableViewController  for edit data

---
+++ CollegeInfo/Controller/College/CollegeDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CollegeDetailViewController.swift
+//  CollegeInfo
+//
+//  Created by Bhumika Patel on 06/05/22.
+//
+
+import UIKit
+
+class CollegeDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
